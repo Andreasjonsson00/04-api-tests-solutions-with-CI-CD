83830e0a27a0c8a2094bf88bcc2a8ab351c02c9e
Andreas <andreasjonsson00@gmail.com>
2026-03-23 16:14:50 +0100

    e2e tests done

--- tests/integration/app.test.js
+++ tests/integration/app.test.js
@@ -13,34 +13,6 @@ describe("app integration", () => {
     `;
   });  
 
-//   test("Just a demonstration of a real API call, and its drawbacks compared to Mocked API calls", async () => {
-//     let posts = [
-//         {
-//             "id": 1,
-//             "title": "His mother had always taught him",
-//             "body": "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
-//             "tags": [
-//                 "history",
-//                 "american",
-//                 "crime"
-//             ],
-//             "reactions": {
-//                 "likes": 192,
-//                 "dislikes": 25
-//             },
-//             "views": 305,
-//             "userId": 121
-//         }
-//     ]
-
-//     const { fetchPosts } = await import("../../src/api-service.js");
-//     let result = await fetchPosts();
-
-//     expect(result.posts).toEqual(
-//         expect.arrayContaining(posts)
-//     );
-//   })
-
   test("fetchPosts calls the correct URL and returns posts", async () => {
     let postData = { posts: [{ id: 1, title: "Test" }] };
    
